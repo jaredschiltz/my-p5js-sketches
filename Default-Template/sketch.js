4ad0680ac7226972987808f0cd28857da4980b79
Jared Schiltz <jaredschiltz@gmail.com>
2024-01-20 10:51:07 -0700

    add keypressed function

--- Default-Template/sketch.js
+++ Default-Template/sketch.js
@@ -1,8 +1,16 @@
-const WIDTH_HEIGHT = 800
+const WIDTH_HEIGHT = 800;
 function setup() {
   createCanvas(WIDTH_HEIGHT, WIDTH_HEIGHT);
 }
 
 function draw() {
   background(220);
+}
+
+function keyPressed() {
+  if (key == "s") {
+    //saveCanvas("output", "jpg");
+    //image.save("image_mask", "png"); // This saves a transparent mask png file
+    saveGif("output_gif", 10);
+  }
 }
